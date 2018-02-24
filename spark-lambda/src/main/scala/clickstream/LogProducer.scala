435773315a5db5478f3144e5aae062b26abd2c54
Moussi Aymen <moussi.aymen@hotmail.com>
2018-02-24 12:00:04 +0100

    create config loader via new Settings class using Typesafe Config library

---
+++ spark-lambda/src/main/scala/clickstream/LogProducer.scala
@@ -0,0 +1,11 @@
+package clickstream
+
+/**
+  * Created by moussi on 24/02/18.
+  * Extending from the App scala class is a quick Scala shortcut to have the class LogProducer
+  * become runnable without having to define a main function. So essentially the body of the class now becomes
+  * your main executable
+  */
+class LogProducer extends App{
+
+}
--- spark-lambda/src/main/scala/config/Settings.scala
+++ spark-lambda/src/main/scala/config/Settings.scala
@@ -1,10 +1,29 @@
 package config
 
+import com.typesafe.config.ConfigFactory
+
 /**
   * Created by moussi on 24/02/18.
   * scala object guarantee a single instance of this class
   * Singleton
   */
 object Settings {
+  /**
+    * TypeSafe Config library
+    */
+  private val conf = ConfigFactory.load()
 
+  object WebLogGen {
+    private val webLogGen = conf.getConfig("clickstream")
+
+    /**
+      * the reason I'm using lazy vals here is that I don't want Scala to evaluate the value of these properties immediately.
+        I only want it to evaluate the value of the parameter when it gets used.
+      */
+    lazy val records = webLogGen.getInt("records")
+    lazy val timeMultiplier = webLogGen.getInt("time_multiplier")
+    lazy val pages = webLogGen.getInt("pages")
+    lazy val visitors = webLogGen.getInt("visitors")
+    lazy val filePath = webLogGen.getString("file_path")
+  }
 }
