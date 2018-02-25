f64c6929a126ffb933d62e346782c1f9033f1ef8
Moussi Aymen <moussi.aymen@hotmail.com>
2018-02-25 17:43:44 +0100

    using sqlContext to make queries on registred DF

--- spark-lambda/src/main/scala/batch/DataFramesBatchJob.scala
+++ spark-lambda/src/main/scala/batch/DataFramesBatchJob.scala
@@ -3,6 +3,7 @@ package batch
 import config.Settings
 import domain._
 import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.functions._
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -26,6 +27,7 @@ object DataFramesBatchJob {
       * In order use Data frames we need to add spark sql context
       */
     val sqlContext= new SQLContext(sc)
+    import sqlContext.implicits._
 
     val filePath = wlc.filePath
 
@@ -33,39 +35,62 @@ object DataFramesBatchJob {
       * Create RDD from data.tsv file that we genrated with LogProducer class
       */
     val sourceRDD = sc.textFile(filePath)
-    val inputRDD = sourceRDD.flatMap( line => {
+    /**
+      * we let the function defines the column names of our DF
+      * because of using Activity case class
+      */
+    val inputDF = sourceRDD.flatMap( line => {
       buildActivityFromLine(line)
-    })
+    }).toDF()
 
     /**
-      * create an RDD with composite Key (product:String, Long) and Value activity
-      * Inout = Activity => output ((product, timestamp), activity)
-      * Cache this RDD I will use it later
+      * select rows/columns of our DF
       */
-    val keyedProducts = inputRDD.keyBy( input => (input.product, input.timestamp_hour)).cache()
-    /**
-      * map Values of Tuple keyed
-      * MapValue keep the same key and map the value
-      */
-    val visitorsByProduct = keyedProducts.mapValues(activity => activity.visitor)
-      .distinct().countByKey()
+    val df = inputDF.select(
+      add_months(from_unixtime(inputDF("timestamp_hour") / 1000), 1).as("timestamp_hour"),
+      inputDF("referrer"), inputDF("action"), inputDF("prevPage"), inputDF("page"), inputDF("visitor"), inputDF("product")
+    ).cache()
 
+    df.registerTempTable("activity")
 
     /**
-      * Reduce By Key foreach key we accumulate indices
+      * apply sql statement to our registeredTEmpTable
       */
-    val activityByProduct = keyedProducts.mapValues { activity =>
-      activity.action match {
-        case "purchase" => (1, 0, 0)
-        case "add_to_cart" => (0, 1, 0)
-        case "page_view" => (0, 0, 1)
-      }
-    }
-      .reduceByKey((a, b) => (a._1 + b._1, a._2 + b._2, a._3 + b._3))
 
-    println("********* visitorsByProduct ***********")
-    visitorsByProduct.foreach(println)
-    println("********* activityByProduct ***********")
-    activityByProduct.foreach(println)
+    val visitorsByProduct = sqlContext.sql(
+      """SELECT product, timestamp_hour, COUNT(DISTINCT visitor) as unique_visitors
+        |FROM activity GROUP BY product, timestamp_hour
+      """.stripMargin)
+
+    visitorsByProduct.printSchema()
+    //visitorsByProduct.foreach(println)
+
+    val activityByProduct = sqlContext.sql("""SELECT
+                                            product,
+                                            timestamp_hour,
+                                            sum(case when action = 'purchase' then 1 else 0 end) as purchase_count,
+                                            sum(case when action = 'add_to_cart' then 1 else 0 end) as add_to_cart_count,
+                                            sum(case when action = 'page_view' then 1 else 0 end) as page_view_count
+                                            from activity
+                                            group by product, timestamp_hour """).cache()
+
+    activityByProduct.registerTempTable("activityProduct")
+
+    /**
+      * Define a custom function via sqlContext and used with you sql statement
+      *
+      */
+
+    sqlContext.udf.register("UnderExposed", (pageViewCount: Long, purshaseCount: Long) => if (purshaseCount == 0) 0 else pageViewCount/purshaseCount)
+    val underExposedProducts = sqlContext.sql("""SELECT
+                                            product,
+                                            timestamp_hour,
+                                            UnderExposed(page_view_count, purchase_count) as negative_exposure
+                                            from activityProduct
+                                            ORDER BY negative_exposure
+                                            LIMIT 5 """)
+    underExposedProducts.printSchema()
+    underExposedProducts.foreach(println)
+
   }
 }
